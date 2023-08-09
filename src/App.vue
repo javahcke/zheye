7977f59001bd0d803293eb45deb4715b0471b407
yuming_yang <1024217025@qq.com>
2023-08-09 18:18:15 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" id="app">
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: 'test1的专栏描述',
+    avatar: 'https://www.gszhjr.com.cn/fastdfs/group1/M00/01/83/rKgBEmS2R8OAIS_xAAAUBA_zEtQ365.png'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: 'test2的专栏描述',
+    avatar: 'https://www.gszhjr.com.cn/fastdfs/group1/M00/00/5C/rKgBEmLwwsqAWg0QAAAMNeDMYvo143.png'
+  }
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :src="column.avatar">
+      <h5>{{ column.title }}</h5>
+      <p>{{ column.description }}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType< ColumnProps[] >,
+      required: true
+    }
+  },
+  setup () {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+  }
+})
+</script>
