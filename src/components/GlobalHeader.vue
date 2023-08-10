d240fb2cbb5626ad6b025534f29119cb411c2825
yuming_yang <1024217025@qq.com>
2023-08-10 18:26:48 +0800

    1

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4">
+    <a class="navbar-brand" href="#">者也专栏</a>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+// export interface GlobalHeaderState {
+//   user: UserProps;
+//   collapsed: boolean;
+// } 
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  },
+  setup() {
+    
+  },
+})
+</script>
