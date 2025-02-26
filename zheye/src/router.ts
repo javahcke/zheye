49ffede7b04ccb5d72d53e9ae79acdfc3d607c79
yuming_yang <1024217025@qq.com>
2025-02-26 18:19:30 +0800

    第6章 第9节

--- zheye/src/router.ts
+++ zheye/src/router.ts
@@ -1,6 +1,6 @@
-import { createRouter, createMemoryHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
